d42776a14405b6581e49b55f5e5be0a5c05167c8
manurocck <manuelnielsen.5g@gmail.com>
2022-09-10 15:05:11 -0300

    Elimino componente PAGO-EFECTIVO
    - Ahora incluido llamado MEDIO-DE-PAGO

--- Trabajos Prácticos/TP 6 Evaluable/ISW-G5-2022_TP_6/src/app/app.module.ts
+++ Trabajos Prácticos/TP 6 Evaluable/ISW-G5-2022_TP_6/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { PagoEfectivoComponent } from './components/pago-efectivo/pago-efectivo.component';
 import { DireccionEnvioComponent } from './components/direccion-envio/direccion-envio.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MedioDePagoComponent } from './medio-de-pago/medio-de-pago.component';
@@ -13,7 +12,6 @@ import { MedioDePagoComponent } from './medio-de-pago/medio-de-pago.component';
   declarations: [
     AppComponent,
     DireccionEnvioComponent,
-    PagoEfectivoComponent,
     MedioDePagoComponent,
 
   ],
